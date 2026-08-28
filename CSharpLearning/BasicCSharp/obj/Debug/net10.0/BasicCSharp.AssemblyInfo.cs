659d6849c70f168c91e5c16201934b4dabadbb3d
Boktiar Mahamud <boktiarmahamud@gmail.com>
2026-08-28 16:45:47 +0600

    Enum example

--- CSharpLearning/BasicCSharp/obj/Debug/net10.0/BasicCSharp.AssemblyInfo.cs
+++ CSharpLearning/BasicCSharp/obj/Debug/net10.0/BasicCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc4fa074ff0e5dc72873dff46fa06b621cca933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b46d02a2307d768fead666168ee1ccb8775b88")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
