ac2b353179751445e534aa46a2b5f227250e1e73
Boktiar Mahamud <boktiarmahamud@gmail.com>
2026-08-30 23:30:19 +0600

    implement filehandling opereation like copy, move, directory access, delete directory, and handling exception

---
+++ CSharpLearning/BasicCSharp/DirectoryGetFiles.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection.Metadata;
+
+namespace BasicCSharp
+{
+    internal class DirectoryGetFiles
+    {
+        public static void Main(string[] args)
+        {
+            // Directory .GetFiles() method is used to get the list of files in a directory
+            string directoryPath = "I://Documents";
+            string[] files = Directory.GetFiles(directoryPath);
+
+            foreach(string file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            // copy data one file to another file
+            string sourceFilePath = "I://Documents//example.txt";
+            string destinationFilePath = "I://Documents//example1.txt";
+
+            File.Copy(sourceFilePath, destinationFilePath, true);
+            Console.WriteLine("Copied data from " + sourceFilePath + " to " + destinationFilePath);
+
+            // Handling exceptions
+
+            Console.WriteLine("Handoling exceptions");
+            string filePath = "I://Document//example.txt";
+            try
+            {
+                string content = File.ReadAllText(filePath);
+                Console.WriteLine("Content of the file: " + content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"File not found: {ex.Message}");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine($"Access denied: {ex.Message}");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"IO error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
